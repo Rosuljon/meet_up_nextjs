200af26fb9d62b997abb5c6832dcf8799c1b9ad0
Ronny <ikramjanov97@mail.ru>
2022-11-24 10:52:52 +0900

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const Dummy = [
+  {
+    id: "m1",
+    title: "coding meetups",
+    image:
+      "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y29kaW5nfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    address: "namangan uzbekistan",
+    description: 'this is a coding meetups'
+  },
+  {
+    id: "m1",
+    title: "coding meetups",
+    image:
+      "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y29kaW5nfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    address: "namangan uzbekistan",
+    description: 'this is a coding meetups'
+  },
+];
+
+const Homepage = () => {
+  return (
+    <>
+      <MeetupList meetups={Dummy} />
+    </>
+  );
+};
+
+export default Homepage;
