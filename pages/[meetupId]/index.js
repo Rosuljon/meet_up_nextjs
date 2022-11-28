c83c9b3ad4d22207cf12fd34fd89c28d171aec27
Ronny <ikramjanov97@mail.ru>
2022-11-28 16:09:03 +0900

    finished DB

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,4 +1,5 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+import mongoose from "mongoose";
 
 function MeetupDetails(props) {
   return (
@@ -11,34 +12,35 @@ function MeetupDetails(props) {
   );
 }
 export async function getStaticPaths() {
+  const conn = await mongoose.connect('mongodb+srv://RonnyDev7:RonnyDev7@meetup.zzxbj4g.mongodb.net/meetup?retryWrites=true&w=majority');
+  console.log(`connected3: ${conn.connection.host}`);
+  const db = mongoose.connection.db;
+  const meetupsCollection = db.collection('meetup')
+  const meetups = await meetupsCollection.find({},{_id: 1}).toArray();
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m12",
-        },
-      },
-    ],
+    paths: meetups.map(meetup => ({
+      params: {meetupId: meetup._id.toString()},
+    })),
   };
 }
 
 export async function getStaticProps(content) {
   const meetupId = content.params.meetupId;
+
+  const conn = await mongoose.connect('mongodb+srv://RonnyDev7:RonnyDev7@meetup.zzxbj4g.mongodb.net/meetup?retryWrites=true&w=majority');
+  console.log(`connected4: ${conn.connection.host}`);
+  const db = mongoose.connection.db;
+  const meetupsCollection = db.collection('meetup')
+  const selectedMeetup = await meetupsCollection.findOne({_id: mongoose.Types.ObjectId(meetupId)})
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        image:
-          "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y29kaW5nfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
-        title: "a first meetup",
-        address: "some street 5, some sity",
-        desc: "meetup describtion",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description
       },
     },
   };
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList";
+import mongoose, { connection } from 'mongoose';
 
 const Dummy = [
   {
@@ -34,9 +35,22 @@ const Homepage = (props) => {
 // }
 export async function getStaticProps() {
   //fetch data from an Api
+  const conn = await mongoose.connect('mongodb+srv://RonnyDev7:RonnyDev7@meetup.zzxbj4g.mongodb.net/meetup?retryWrites=true&w=majority');
+        console.log(`connected1: ${conn.connection.host}`);
+        const db = mongoose.connection.db;
+        const meetupsCollection = db.collection('meetup')
+        const meetups = meetupsCollection.find().toArray()
+        
+        
   return {
     props: {
-      meetups: Dummy,
+      meetups: (await meetups).map(meetup => ({
+        title : meetup.title,
+        address: meetup.address,
+        desc: meetup.description,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      }))
     },
     revalidate: 1,
   };
